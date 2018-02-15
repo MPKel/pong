148dd1d0e10ec353c95f6474edefc75dfcb677df
Michael <mpkelley08@gmail.com>
2018-02-14 20:37:10 -0500

    visuals added

--- app.js
+++ app.js
@@ -1,21 +1,13 @@
+const table = document.getElementById('table');
+var context = table.getContext('2d');
 
-
-function onReady() {
-
-  const table = document.getElementById('table');
-  var context = table.getContext('2d');
-
-      // do cool things with the context
-      // context.font = '40pt Calibri';
-      // context.fillStyle = 'blue';
-      // context.fillText('Hello World!', 150, 100);
+function render() {
 
       //draw the net
       context.beginPath();
       context.moveTo(300, 0);
       context.lineTo(300, 525);
       context.lineWidth = 10;
-      // context.strokeStyle = 'rgb(32, 247, 128)'
       context.stroke();
 
       //draw net holders
@@ -33,19 +25,60 @@ function onReady() {
       context.strokeStyle = 'black';
       context.stroke();
 
-      var paddle1 = document.getElementById("paddle");
-      context.drawImage(paddle1, 0, 262, 50, 50);
+      //draw Player paddle
+      var player1 = new Paddle(50, 50, 0, 262);
+      player1.render();
 
-      var paddle2 = document.getElementById("paddle");
-      context.drawImage(paddle2, 550, 262, 50, 50);
+      //draw computer paddle
+      var computer = new Paddle(50, 50, 550, 262);
+      computer.render();
 
 
-
+      //draw the ball
+      var gameBall = new Ball(200, 200, 10, 0, Math.PI * 2, false);
+      gameBall.render();
 
  }
 
+ class Ball {
+  constructor(xloc, yloc, radius, sAngle, eAngle, cClock) {
+    this.xloc = xloc;
+    this.yloc = yloc;
+    this.radius = radius;
+    this.sAngle = sAngle;
+    this.eAngle = eAngle;
+    this.cClock = cClock;
+  }
+
+   render(){
+    context.beginPath();
+    context.arc(this.xloc, this.yloc, this.radius, this.sAngle, this.eAngle, this.cClock);
+    context.fillStyle = "white";
+    context.fill();
+    context.lineWidth = 2;
+    context.stroke();
+  }
+}
+
+
+
+ class Paddle {
+  constructor(height, width, xloc, yloc) {
+    this.dom = document.getElementById("paddle");
+    this.height = height;
+    this.width = width;
+    this.xloc = xloc;
+    this.yloc = yloc;
+  }
+
+  render(){
+    context.drawImage(this.dom, this.xloc, this.yloc, this.height, this.width);
+  }
+}
+
+
 
 window.onload = function() {
-   alert("The window has loaded!");
-   onReady();
+
+   render();
  };
